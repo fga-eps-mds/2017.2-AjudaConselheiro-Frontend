17a60cf612fe013d2b1ea4ce6a65201af27bead3
TPFChaos <thiagoiferreira@outlook.com>
2017-09-17 02:06:56 -0300

    Minor: app.module.ts import alteration to add checklists/ folder to component adress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-import { ChecklistoneComponent } from './checklistone/checklistone.component';
-import { ChecklisttwoComponent } from './checklisttwo/checklisttwo.component';
-import { ChecklistthreeComponent } from './checklistthree/checklistthree.component';
+import { ChecklistoneComponent } from './checklists/checklistone/checklistone.component';
+import { ChecklisttwoComponent } from './checklists/checklisttwo/checklisttwo.component';
+import { ChecklistthreeComponent } from './checklists/checklistthree/checklistthree.component';
 @NgModule({
   declarations: [
     AppComponent,
