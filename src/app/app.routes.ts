7cf3ff66c80eb014f3bd6069f6f7a4dc818ab5b9
joao4018 <joao4018@gmail.com>
2017-09-18 20:23:23 -0300

    Minor: initial functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes} from '@angular/router';
 
 import { SchedulingVisitComponent } from './scheduling/scheduling-visit/scheduling-visit.component';
 import { SchedulingComponent } from './scheduling/scheduling-meeting/scheduling-meeting.component';
+import { SchedulingHomeComponent } from './scheduling/scheduling-home/scheduling-home.component';
 
 
 
 export const ROUTES: Routes = [
     {path: 'agendamento/reuniao', component: SchedulingComponent},
+    {path: 'agendamento', component: SchedulingHomeComponent},
     {path: 'agendamento/visita', component: SchedulingVisitComponent}
   ];
