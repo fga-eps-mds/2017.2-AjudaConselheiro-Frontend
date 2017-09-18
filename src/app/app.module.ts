ef1b29e39d0f6876d26aba2bbfa0f9998e9452dd
Gabriel Viana <gabrielv14@gmail.com>
2017-09-17 21:22:50 -0300

    Minor: (after merge) import FormsModule duplicated fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ChecklistoneComponent } from './checklists/checklistone/checklistone.component';
 import { ChecklisttwoComponent } from './checklists/checklisttwo/checklisttwo.component';
