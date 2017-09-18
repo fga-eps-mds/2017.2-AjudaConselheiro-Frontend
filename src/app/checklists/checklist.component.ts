6881b44b473676ded1cf809bc0acf3336e4f8a12
pAmanda <amandoca_779@hotmail.com>
2017-09-18 02:01:55 -0300

    Creating checklist component.

---
+++ src/app/checklists/checklist.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'checklist',
+    templateUrl: 'checklist.componet.html'
+})
+export class ChecklistComponent{}
