7d1aa5f14301d834f44a7144f3c368b44f240446
gustavocarvalho1002 <guga_carvalho12@hotmail.com>
2017-09-26 00:47:57 -0300

    Minor: bug fix(file path problem)

--- src/app/scheduling/index.ts
+++ src/app/scheduling/index.ts
@@ -1,3 +1,3 @@
-export * from '/home/carvalho/MDS - AjudaConselheiro/2017.2-AjudaConselheiro-Frontend/src/app/scheduling/scheduling-home/scheduling-home.component';
-export * from '/home/carvalho/MDS - AjudaConselheiro/2017.2-AjudaConselheiro-Frontend/src/app/scheduling/create/create-scheduling.component';
+export * from './../scheduling/scheduling-home/scheduling-home.component';
+export * from './../scheduling/create/create-scheduling.component';
 export * from './scheduling.module';
