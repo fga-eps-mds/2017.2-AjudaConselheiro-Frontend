4132d1681a9f56367dc504e64ff928956935d55f
pAmanda <amandoca_779@hotmail.com>
2017-10-17 15:33:14 -0200

    Minor: Making changes in buttons, user model and create component.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,12 @@
 export class User {
 
-    constructor(
-      public CEP?: string,
-      public cod?: number,
-      public email?: string,
-      public nomeCompleto?: string,
-      public nomeUsuario?: string,
-      public senha?: string,
-    ) {}
+    constructor() {}
+
+    public cep: string;
+    public cod?: number;
+    public email: string;
+    public nomeCompleto: string;
+    public nomeUsuario: string;
+    public senha: string;
 
 }
