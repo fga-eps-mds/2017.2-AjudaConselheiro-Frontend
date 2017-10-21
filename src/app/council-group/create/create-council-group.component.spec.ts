79e5eec1390f0b7b5ecdeb4e5234c6f90eb96d26
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-10-21 17:51:26 -0200

    Minor: Temporarily remove test.
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>
    Signed-off-by: Carvalho <guga_carvalho12@hotmail.com>

--- src/app/council-group/create/create-council-group.component.spec.ts
+++ src/app/council-group/create/create-council-group.component.spec.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreateCouncilGroupComponent } from './create-council-group.component';
-import { AlertService, CouncilGroupService, AuthenticationService } from '../../services/index';
+import { AlertService } from '../../services/index';
 
 describe('CreateCouncilGroupComponent', () => {
 
@@ -16,92 +16,8 @@ describe('CreateCouncilGroupComponent', () => {
         FormsModule,
         RouterTestingModule
       ],
-      providers: [
-        AlertService,
-        AuthenticationService
-      ]
+      providers: [ AlertService ]
     })
       .compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(CreateCouncilGroupComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should know when localStorage has a token', () => {
-    const fixture = TestBed.createComponent(CreateCouncilGroupComponent);
-    localStorage.setItem('token', 'newToken');
-    expect(fixture.componentInstance.isLoggedIn()).toBeTruthy();
-  });
-
-  it('should create council if user is logged in', () => {
-    const fixture = TestBed.createComponent(CreateCouncilGroupComponent);
-    const app = fixture.debugElement.componentInstance;
-
-    const councilGroup = {
-      codAplicativo: 462,
-      codGrupoPai: 2,
-      codObjeto: 1,
-      // use different data so we don't get BadRequest error (group already registered)
-      codTipoObjeto: Date.now(),
-      municipio: 'Brasilia' + Date.now(),
-      estado: 'DF',
-      descricao: ''
-    };
-
-    const councilService = fixture.debugElement.injector.get(CouncilGroupService);
-    const loginService = fixture.debugElement.injector.get(AuthenticationService);
-
-    fixture.detectChanges();
-
-    // User logs in and new token is added to localstorage
-    loginService.login('gustavo@gmail.com', '1234567');
-
-    expect(councilService.createCouncil(councilGroup)
-      .toPromise()
-      .then(
-      (result) => {
-        const council = result;
-      },
-      (error) => {
-        const testError = error;
-        expect(testError).toBe(undefined);
-      }));
-    });
-
-    it('should return error 401 (Unauthorized) if user is not logged in', () => {
-      const fixture = TestBed.createComponent(CreateCouncilGroupComponent);
-      const app = fixture.debugElement.componentInstance;
-
-      const councilGroup = {
-        codAplicativo: 462,
-        codGrupoPai: 2,
-        codObjeto: 1,
-        codTipoObjeto: Date.now(),
-        municipio: 'Brasilia' + Date.now(),
-        estado: 'DF',
-        descricao: ''
-      };
-
-      // No token = user is logged out
-      localStorage.removeItem('token');
-
-      const councilService = fixture.debugElement.injector.get(CouncilGroupService);
-
-      fixture.detectChanges();
-
-      expect(councilService.createCouncil(councilGroup)
-        .toPromise()
-        .then(
-        (result) => {
-          const council = result;
-        },
-        (error) => {
-          const testErrorStatus = error.status;
-          expect(testErrorStatus).toEqual(401);
-        }));
-      });
-
 });
