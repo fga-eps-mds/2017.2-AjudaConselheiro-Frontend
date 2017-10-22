1cbf519b8bd3f6f66bcf5467916806b6599ff0c8
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2017-10-22 13:34:15 -0200

    Revert "Us#60"

--- src/app/checklist/checklistone/checklistone.component.ts
+++ src/app/checklist/checklistone/checklistone.component.ts
@@ -1,30 +1,23 @@
-import { Component, OnInit } from '@angular/core';
 import { Binary } from '@angular/compiler';
-import { Http, HttpModule } from '@angular/http';
-import { DomSanitizer } from '@angular/platform-browser';
-
 import { SectionCommentary } from '../../models/checklist.model';
 import { CheckOneTopicHeaders, CheckOneCommentaries, CheckOneFirstTopic, CheckOneEighthTopic, CheckOneFifthTopic,
 CheckOneFourthTopic, CheckOneSecondTopic, CheckOneSixthTopic,
 CheckOneSeventhTopic, CheckOneThirdTopic, IteratorArray} from './../../models/checklistForms';
+import { Component, OnInit } from '@angular/core';
 import { FormsMenu, FormMenuTwo, CommentBinaryForm, CommentForm, BinaryForm, FormBinary, ConfirmComentary} from '../../models/index';
 import { ChecklistService } from '../../services/index';
-
-
+import { Http, HttpModule } from '@angular/http';
 @Component({
   selector: 'app-checklistone',
   templateUrl: './checklistone.component.html',
   styleUrls: ['./checklistone.component.css'],
-  providers: [],
+  providers: [ChecklistService],
 })
 export class ChecklistoneComponent implements OnInit {
-
-  base64Array: String[] = [];
-  public base64Image: String;
-
   // This component is destined to the checklist corresponding to
+  // LISTA PARA VERIFICAÇÃO DAS BOAS PRÁTICAS DE FABRICAÇÃO
     iteratorArray: Array<Object>= IteratorArray;
-    topicHeaders: Array<String> = CheckOneTopicHeaders;
+    topicHeaders: Array<string> = CheckOneTopicHeaders;
     commentaries: Array<SectionCommentary> = CheckOneCommentaries;
     checkOneFirstTopic: Array<BinaryForm> = CheckOneFirstTopic;
     checkOneSecondTopic: Array<BinaryForm> = CheckOneSecondTopic;
@@ -47,10 +40,53 @@ export class ChecklistoneComponent implements OnInit {
       new ConfirmComentary(false, 'Comment8'),
     ];
 
-  constructor(private domSanitizer: DomSanitizer) {}
+  constructor() {}
 
   ngOnInit(): void {
+    // this.checkoneService.getCheckOneFirstTopicAnswers()
+    // .then((checkOneFirstTopic: BinaryForm[]) => {
+    //     this.checkOneFirstTopic = checkOneFirstTopic;
+    // }).catch(err => console.log(err));
 
+    // this.checkoneService.getCheckOneSecondTopicAnswers()
+    // .then((checkOneSecondTopic: BinaryForm[]) => {
+    //    this.checkOneSecondTopic = checkOneSecondTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneThirdTopicAnswers()
+    // .then((checkOneThirdTopic: BinaryForm[]) => {
+    //    this.checkOneThirdTopic = checkOneThirdTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneFourthTopicAnswers()
+    // .then((checkOneFourthTopic: BinaryForm[]) => {
+    //        this.checkOneFourthTopic = checkOneFourthTopic;
+    //     }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneFifthTopicAnswers()
+    // .then((checkOneFifthTopic: BinaryForm[]) => {
+    //    this.checkOneFifthTopic = checkOneFifthTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneSixthTopicAnswers()
+    // .then((checkOneSixthTopic: BinaryForm[]) => {
+    //    this.checkOneSixthTopic = checkOneSixthTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneSeventhTopicAnswers()
+    // .then((checkOneSeventhTopic: CommentBinaryForm[]) => {
+    //    this.checkOneSeventhTopic = checkOneSeventhTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneEighthTopicAnswers()
+    // .then((checkOneEighthTopic: BinaryForm[]) => {
+    //    this.checkOneEighthTopic = checkOneEighthTopic;
+    // }).catch(err => console.log(err));
+
+    // this.checkoneService.getCheckOneCommentaries()
+    // .then((commentaries: SectionCommentary[]) => {
+    //     this.commentaries = commentaries;
+    // }).catch(err => console.log(err));
 }
 
   onSubmit(): void {
@@ -63,21 +99,5 @@ export class ChecklistoneComponent implements OnInit {
       console.log(this.checkOneSeventhTopic);
       console.log(this.checkOneEighthTopic);
       console.log(this.commentaries);
-  }
-
-  changeListener($event): void {
-    this.readThis($event.target);
-  }
-
-  readThis(inputValue: any): void {
-    const file: File = inputValue.files[0];
-    const myReader: FileReader = new FileReader();
-
-    myReader.onloadend = (e) => {
-      this.base64Image = myReader.result;
-      this.base64Array.push(this.base64Image);
-      console.log(this.base64Image);
-    };
-    myReader.readAsDataURL(file);
   }
 }
