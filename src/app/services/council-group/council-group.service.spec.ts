91d10e28732667924b9b5d1481896ca03641908c
joao4018 <joao4018@gmail.com>
2017-10-25 21:52:49 -0200

    Minor: Unit Test "createCouncil"
    Signed-off-by: joao4018 <joao4018@gmail.com>
    Signed-off-by: Thiagotrfm <t_moreira@outlook.com>

--- src/app/services/council-group/council-group.service.spec.ts
+++ src/app/services/council-group/council-group.service.spec.ts
@@ -4,9 +4,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { CouncilGroupService } from './council-group.service';
 import { AlertService } from '../alert/alert.service';
+import { CouncilGroup } from '../../models/index';
+
 
 describe('CouncilGroupService', () => {
+  let councilGroup: CouncilGroup;
+
   beforeEach(() => {
+    councilGroup = new CouncilGroup;
     TestBed.configureTestingModule({
       providers: [CouncilGroupService, AlertService],
       imports: [
@@ -18,5 +23,19 @@ describe('CouncilGroupService', () => {
 
   it('should be created', inject([CouncilGroupService], (service: CouncilGroupService) => {
     expect(service).toBeTruthy();
+  }));
+  fit('should be createde', inject([CouncilGroupService], (service: CouncilGroupService) => {
+    councilGroup.municipio = 'nada';
+    councilGroup.estado = 'otonda';
+    service.createCouncil(councilGroup);
+    const temp = service.body;
+    const teste = {
+      'codAplicativo': 462,
+      'codGrupoPai': 1,
+      'codObjeto': councilGroup.codObjeto,
+      'codTipoObjeto': 1,
+      'descricao': 'CAE-nada-otonda'
+    };
+    expect(temp).toEqual(JSON.stringify(teste));
   }));
 });
