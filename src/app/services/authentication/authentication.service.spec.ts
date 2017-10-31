63f4e85eea1180f75124e4f25d5926c15b70146d
MFire30 <matheus_oliveira30@hotmail.com>
2017-10-31 21:35:40 -0200

    Minor: Initial testing for Login in authentication service
    Signed-off-by: MFire30 <matheus_oliveira30@hotmail.com>
    Signed-off-by: pAmanda <amandoca_779@hotmail.com>

--- src/app/services/authentication/authentication.service.spec.ts
+++ src/app/services/authentication/authentication.service.spec.ts
@@ -1,28 +1,105 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule } from '@angular/http';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AuthenticationService } from './authentication.service';
-import { AlertService } from '../alert/alert.service';
+import { Http, HttpModule, ConnectionBackend,
+  ResponseOptions, XHRBackend, Response,
+  BaseRequestOptions, RequestOptions, RequestMethod} from '@angular/http';
+import { Headers } from '@angular/http';
 
-describe('AuthenticationService', () => {
+import { TestBed, inject, async} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { Observable } from 'rxjs/Observable';
+
+import { AlertService, AuthenticationService } from '../../services/index';
+import { Post } from '../../models/index';
+
+
+describe('PostService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ AuthenticationService, AlertService ],
       imports: [
         HttpModule,
         RouterTestingModule
-     ]
+      ],
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (mockBackend: MockBackend, defaultOptions: RequestOptions) => {
+            return new Http(mockBackend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        ConnectionBackend,
+        AlertService,
+        AuthenticationService
+      ]
     });
+
+    // Mocking localStorage
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
   });
 
   it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
     expect(service).toBeTruthy();
   }));
 
-  it('should delete token', inject([AuthenticationService], (service: AuthenticationService) => {
-    localStorage.setItem('token', 'newToken');
-    expect(localStorage.hasOwnProperty('token')).toBe(true);
-    service.logout();
-    expect(localStorage.hasOwnProperty('token')).toBe(false);
+  // For getPosts()
+  it('should return a valid user if data are valid',
+    inject([AuthenticationService, MockBackend], (authService, mockBackend) => {
+      const fakeUser = {
+        nomeUsuario: 'Ziegler',
+        nomeCompleto: 'Ziegler Top',
+        cod: 234,
+        email: 'abc@abc.com'
+      };
+
+      const fakeToken = 'FakeToken';
+      const fakeHeader = new Headers({appToken: fakeToken});
+      const fakeData = JSON.stringify(fakeUser);
+
+      const validFakeEmail = 'abc@abc.com';
+      const validFakePassword = 'superS4f3passw0rd';
+
+      // Mocking HTTP connection for this test
+      mockBackend.connections.subscribe((connection: MockConnection) => {
+        const options = new ResponseOptions({ body: fakeData, headers: fakeHeader});
+
+        connection.mockRespond(new Response(options));
+      });
+
+      // Making the request
+      authService.login(validFakeEmail, validFakePassword).subscribe((result) => {
+        const responseToken = result[0];
+        const responseBody = result[1];
+
+        const userResponse = JSON.parse(responseBody._body);
+
+        expect(responseToken).toEqual(fakeToken);
+        expect(userResponse.cod).toEqual(fakeUser.cod);
+        expect(userResponse.email).toEqual(validFakeEmail);
+      });
   }));
 });
