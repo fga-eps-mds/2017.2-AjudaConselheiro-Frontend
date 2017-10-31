0dabcff1c9a1eb1ab9544ffa1421b6261e2c1972
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-10-31 21:51:20 -0200

    Minor: Method to get single post from API
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>
    Signed-off-by: Filipe Dias <lipao.dias@hotmail.com>

--- src/app/services/posts/post.service.ts
+++ src/app/services/posts/post.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
-import { ServicesUtilitiesService, AlertService, UserService } from '../../services/index';
-
 import { Post, User } from '../../models/index';
-
+import { ServicesUtilitiesService } from '../services-utilities.service';
+import { AlertService } from '../../services/alert/alert.service';
+import { UserService } from '../../services/user/user.service';
 
 @Injectable()
 export class PostService extends ServicesUtilitiesService {
-
-  private baseURL = 'http://mobile-aceite.tcu.gov.br:80/appCivicoRS/rest/postagens';
-  private postURL = this.baseURL + '/conteudos';
+  private baseURL = 'http://mobile-aceite.tcu.gov.br:80/appCivicoRS/rest/postagens/';
+  private postURL = this.baseURL + 'conteudos';
 
   // These are 'tipoPostagem' that already been set for this app, in the API
   private incompleteChecklist = 375;
@@ -21,77 +20,93 @@ export class PostService extends ServicesUtilitiesService {
   // Both POST and GET needs these headers
   private headers: Headers = new Headers({
     'Content-Type': 'application/json',
-    'appToken': localStorage.getItem('token'),
-    'appIdentifier': 462
+    appToken: localStorage.getItem('token'),
+    appIdentifier: 462
   });
 
   options: RequestOptions = new RequestOptions({ headers: this.headers });
 
-  constructor(private http: Http,
+  constructor(
+    private http: Http,
     private alertService: AlertService,
-    private userService: UserService) {
-      super();
+    private userService: UserService
+  ) {
+    super();
   }
 
-    getPosts(): Observable<Post> {
-      const thereIsToken = localStorage.getItem('token');
+  getPosts(): Observable<Post> {
+    const thereIsToken = localStorage.getItem('token');
 
-      if (thereIsToken) {
-        return this.http.get(this.baseURL, this.options)
-          .map(this.extractData)
-          .catch(this.handleError);
-      } else {
-        this.alertService.warn('Você precisa estar logado');
-      }
+    if (thereIsToken) {
+      return this.http
+        .get(this.baseURL, this.options)
+        .map(this.extractData)
+        .catch(this.handleError);
+    } else {
+      this.alertService.warn('Você precisa estar logado');
+    }
+  }
+
+  getPost(cod: number): Observable<Post> {
+    const appToken = localStorage.getItem('token');
+
+    if (appToken) {
+      return this.http
+        .get(this.baseURL + cod, this.options)
+        .map(this.extractData)
+        .catch(this.handleError);
+    } else {
+      this.alertService.warn('Você precisa estar logado');
+    }
+  }
+
+  savePost(data: any): Observable<Post> {
+    const codUser = this.getUserCod();
+
+    if (codUser) {
+      const body = this.formatPostBody(data, codUser, this.testChecklist);
+
+      return this.http
+        .post(this.postURL, body, this.options)
+        .map(this.extractData)
+        .catch(this.handleError);
+    } else {
+      console.error('You cannot create a post without login first!');
+      this.alertService.warn('Você precisa estar logado');
     }
 
-    savePost(data: any): Observable<Post> {
-      const codUser = this.getUserCod();
+    return new Observable<Post>();
+  }
 
-      if (codUser) {
-        const body = this.formatPostBody(data, codUser, this.testChecklist);
-
-        return this.http.post(this.postURL, body, this.options)
-          .map(this.extractData)
-          .catch(this.handleError);
-      } else {
-        console.error('You cannot create a post without login first!');
-        this.alertService.warn('Você precisa estar logado');
-      }
-
-      return new Observable<Post>();
-    }
-
-    // The function below just take any JS Object and transforms it to a valid JSON
-    private formatPostBody(jsonData: any, codUser: number, codPost: number) {
-      const validBody = {
-        'conteudo': {
-          'JSON': jsonData
+  // The function below just take any JS Object and transforms it to a valid JSON
+  private formatPostBody(jsonData: any, codUser: number, codPost: number) {
+    const validBody = {
+      conteudo: {
+        JSON: jsonData
+      },
+      postagem: {
+        autor: {
+          codPessoa: codUser
         },
-        'postagem': {
-          'autor': {
-            'codPessoa': codUser
-          },
-          'tipo': {
-            'codTipoPostagem': codPost
-          }
+        tipo: {
+          codTipoPostagem: codPost
         }
-      };
-
-      return JSON.stringify(validBody);
-    }
-
-    // This function checks if there's a logged user and if it has a 'cod'
-    // Output: The user 'cod' or 'null' if there's no cod
-    private getUserCod() {
-      const user = this.userService.getLoggedUser();
-
-      // Checks if there's a user and if this user has a 'cod' attribute.
-      if (user && 'cod' in user) {
-        return user.cod;
       }
+    };
 
-      return null;
+    return JSON.stringify(validBody);
+  }
+
+  // This function checks if there's a logged user and if it has a 'cod'
+  // Output: The user 'cod' or 'null' if there's no cod
+  private getUserCod() {
+    const user = this.userService.getLoggedUser();
+
+    // Checks if there's a user and if this user has a 'cod' attribute.
+    if (user && 'cod' in user) {
+      return user.cod;
     }
-}
 
+    return null;
+  }
+}
