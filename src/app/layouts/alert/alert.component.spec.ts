dbd50f1869ee3183205b832179dbf991df59fc35
Caio Azevedo <caioba19@gmail.com>
2017-11-08 17:48:27 -0200

    Minor: Testing method removeAlert from Alert Component
    Signed-off-by: Caio Azevedo <caioba19@gmail.com>
    Signed-off-by: pAmanda <amandoca_779@hotmail.com>

--- src/app/layouts/alert/alert.component.spec.ts
+++ src/app/layouts/alert/alert.component.spec.ts
@@ -1,20 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockBackend } from '@angular/http/testing';
 
 import { AlertComponent } from './alert.component';
 import { AlertService } from '../../services/alert/alert.service';
+import { Alert, AlertType } from '../../models/index';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
+  const fakeAlert = {
+    typeAlert: AlertType.Success,
+    message: 'message'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AlertComponent ],
       imports: [ RouterTestingModule ],
-      providers: [ AlertService ]
+      providers: [ AlertService, MockBackend ]
     })
     .compileComponents();
+
+    const store = {};
+    const mockLocalStorage = {
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      }
+    };
+
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
   }));
 
   beforeEach(() => {
@@ -26,4 +42,10 @@ describe('AlertComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should remove alert', () => {
+    localStorage.setItem('token', 'fakeToken');
+    component.removeAlert(this.fakeAlert);
+  });
+
 });
