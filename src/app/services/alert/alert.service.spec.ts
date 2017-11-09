b175e98bc9d415bd6d933c05b745ff09cbe0361c
Caio Azevedo <caioba19@gmail.com>
2017-11-09 16:36:52 -0200

    Minor: Testing getAlert and alert function from AlertService
    Signed-off-by: Caio Azevedo <caioba19@gmail.com>
    Signed-off-by: pAmanda <amandoca_779@hotmail.com>

--- src/app/services/alert/alert.service.spec.ts
+++ src/app/services/alert/alert.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AlertService } from './alert.service';
+import { Alert, AlertType } from '../../models/index';
+import { MockBackend } from '@angular/http/testing';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
 
 describe('AlertService', () => {
+  const fakeSubject = new Subject<Alert>();
+  const FakeKeepAfterRouteChange = false;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AlertService],
+      providers: [AlertService,
+                  MockBackend],
       imports: [RouterTestingModule]
     });
   });
 
   it('should be created', inject([AlertService], (service: AlertService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should do get alert', inject([AlertService], (service: AlertService) => {
+    service.getAlert();
+  }));
+
+  it('should be clear', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    expect(this.fakeSubject).toBeUndefined();
   }));
 });
