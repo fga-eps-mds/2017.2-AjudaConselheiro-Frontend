86cb6bb4efea3f0a95c95e3e808905c41eeca196
pAmanda <amandoca_779@hotmail.com>
2017-11-11 12:14:42 -0200

    Major: Leaving the alert service test coverage at 100%.
    Signed-off-by: Caio Azevedo <caioba19@gmail.com>
    Signed-off-by: pAmanda <amandoca_779@hotmail.com>

--- src/app/services/alert/alert.service.spec.ts
+++ src/app/services/alert/alert.service.spec.ts
@@ -6,12 +6,16 @@ import { MockBackend } from '@angular/http/testing';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
-
 describe('AlertService', () => {
   const fakeSubject = new Subject<Alert>();
-  const FakeKeepAfterRouteChange = false;
+  const fakeKeepAfterRouteChange = false;
+  let fakeAlert: Alert;
+  fakeAlert = new Alert();
+  fakeAlert.type = AlertType.Success;
+  fakeAlert.message = 'message';
 
   beforeEach(() => {
+
     TestBed.configureTestingModule({
       providers: [AlertService,
                   MockBackend],
@@ -25,6 +29,36 @@ describe('AlertService', () => {
 
   it('should do get alert', inject([AlertService], (service: AlertService) => {
     service.getAlert();
+  }));
+
+  it('should do alert', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    service.alert( fakeAlert.type, fakeAlert.message, fakeKeepAfterRouteChange);
+    expect(service.subject.next).toBeDefined();
+  }));
+
+  it('should be success alert', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    service.success( fakeAlert.message, fakeKeepAfterRouteChange);
+    expect(service.subject.next).toBeDefined();
+  }));
+
+  it('should be error alert', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    service.error( fakeAlert.message, fakeKeepAfterRouteChange);
+    expect(service.subject.next).toBeDefined();
+  }));
+
+  it('should be warn alert', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    service.warn( fakeAlert.message, fakeKeepAfterRouteChange);
+    expect(service.subject.next).toBeDefined();
+  }));
+
+  it('should be info alert', inject([AlertService], (service: AlertService) => {
+    service.clear();
+    service.info( fakeAlert.message, fakeKeepAfterRouteChange);
+    expect(service.subject.next).toBeDefined();
   }));
 
   it('should be clear', inject([AlertService], (service: AlertService) => {
