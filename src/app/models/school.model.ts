a3bca37ad7aa73969af725afa1bf421e1432d579
Carvalho <guga_carvalho12@hotmail.com>
2017-11-17 12:04:15 -0200

    Minor: Improving codebeat coverage
    Signed-off-by: Carvalho <guga_carvalho12@hotmail.com>
    Signed-off-by: Filipe Dias <lipão.dias@hotmail.com>

--- src/app/models/school.model.ts
+++ src/app/models/school.model.ts
@@ -1,9 +1,8 @@
 export class School {
-  constructor(
-      public code?: string,
-      public city?: string,
-      public state?: string,
-      public name?: string,
-      public situation?: string
-  ) {}
+  constructor() {}
+    public code?: string;
+    public city?: string;
+    public state?: string;
+    public name?: string;
+    public situation?: string;
 }
