8372d175da173f1dcd7a90bdb9b5566958f1bf16
MFire30 <matheus_oliveira30@hotmail.com>
2017-11-18 16:43:53 -0200

    Minor: Finishing unit tests for new component

--- src/app/user/recover-password/recover-password.component.spec.ts
+++ src/app/user/recover-password/recover-password.component.spec.ts
@@ -1,13 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed,
+  inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, ResponseOptions, Response } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RecoverPasswordComponent } from './recover-password.component';
 
 import { UserService, AlertService, ProfileService,
   AuthenticationService } from '../../services/index';
+import { Observable } from 'rxjs/Observable';
 
 describe('RecoverPasswordComponent', () => {
   let component: RecoverPasswordComponent;
@@ -40,7 +43,101 @@ describe('RecoverPasswordComponent', () => {
     fixture.detectChanges();
   });
 
+  // For componente instatiation
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // For recoverPassword()
+  it('recoverPassword() should call sendNewPassword with valid email',
+    inject([UserService], (service) => {
+
+    // Setting form value and update new changes
+    component.form.setValue({
+      email: 'email@abc.com',
+    });
+    fixture.detectChanges();
+
+    // Generating a fake Observable as response from service
+    const fakeResOptions = new ResponseOptions({body: {}});
+    const fakeRes = new Response(fakeResOptions);
+    const spy = spyOn(service, 'sendNewPassword').and.returnValue(
+      // Generates a valid response for the service
+      Observable.of(fakeRes)
+    );
+
+    // Call method and observe result
+    component.recoverPassword();
+    expect(service.sendNewPassword).toHaveBeenCalled();
+
+  }));
+
+  it('recoverPassword() should handle sendNewPassword errors',
+    inject([UserService], (service) => {
+
+    component.form.setValue({
+      email: 'email@abc.com',
+    });
+    fixture.detectChanges();
+
+    const fakeResOptions = new ResponseOptions({body: {}});
+    const fakeRes = new Response(fakeResOptions);
+    const spy = spyOn(service, 'sendNewPassword').and.returnValue(
+      Observable.throw(fakeRes)
+    );
+
+    component.recoverPassword();
+    expect(service.sendNewPassword).toHaveBeenCalled();
+
+  }));
+
+  it('recoverPassword() should reset forms on sucess',
+    inject([UserService], (service) => {
+
+    component.form.setValue({
+      email: 'email@abc.com',
+    });
+    fixture.detectChanges();
+
+
+    const fakeResOptions = new ResponseOptions({body: {}});
+    const fakeRes = new Response(fakeResOptions);
+    const spyService = spyOn(service, 'sendNewPassword').and.returnValue(
+      Observable.of(fakeRes)
+    );
+
+    // Creates a reset() spy and update changes
+    const spyForm = spyOn(component.form, 'reset').and.callThrough();
+    component.recoverPassword();
+    fixture.detectChanges();
+
+    expect(component.form.reset).toHaveBeenCalled();
+
+    const emailValue = component.form.get('email') as any;
+    expect(emailValue._value).toBeNull();
+
+  }));
+
+  it('recoverPassword() should reset forms on fail',
+    inject([UserService], (service) => {
+
+    component.form.setValue({
+      email: 'email@abc.com',
+    });
+    fixture.detectChanges();
+
+    const fakeResOptions = new ResponseOptions({body: {}});
+    const fakeRes = new Response(fakeResOptions);
+    const spyService = spyOn(service, 'sendNewPassword').and.returnValue(
+      Observable.throw(fakeRes)
+    );
+
+    const spyForm = spyOn(component.form, 'reset').and.callThrough();
+    component.recoverPassword();
+    fixture.detectChanges();
+    expect(component.form.reset).toHaveBeenCalled();
+
+    const emailValue = component.form.get('email') as any;
+    expect(emailValue._value).toBeNull();
+  }));
 });
