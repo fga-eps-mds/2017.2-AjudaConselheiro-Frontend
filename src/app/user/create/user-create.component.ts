952dbea4697f2988e5cdea065c9c437477da8663
joao4018 <joao4018@gmail.com>
2017-11-21 12:53:09 -0200

    Major: Moving CPf for create component
    Signed-off-by: joao4018 <joao4018@gmail.com>
    Signed-off-by: Filipe Dias <lipao.dias@hotmail.com>

--- src/app/user/create/user-create.component.ts
+++ src/app/user/create/user-create.component.ts
@@ -1,3 +1,4 @@
+import { ProfileService } from './../../services/profile/profile.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http } from '@angular/http';
@@ -16,15 +17,17 @@ export class UserCreateComponent implements OnInit {
 
   @ViewChild('formUser') formUser: NgForm;
   user: User;
-
+  data: string;
   constructor(
     private userService: UserService,
     private router: Router,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private profileService: ProfileService
   ) { }
 
   ngOnInit() {
     this.user = new User();
+    this.data = null;
   }
 
   matchPassword(confirmPassword: string, password: string): boolean {
@@ -57,6 +60,7 @@ export class UserCreateComponent implements OnInit {
         .subscribe(
         result => {
           this.result();
+          this.savePosts(this.data);
         },
         error => {
           this.error(error.status);
@@ -65,4 +69,76 @@ export class UserCreateComponent implements OnInit {
       this.alertService.warn('Senhas não conferem!');
     }
   }
+  savePosts(strCPF: string) {
+    
+        if (this.testCPF(strCPF) === true) {
+          const userData = this.userService.getLoggedUser();
+          const userCod = userData.cod;
+    
+          this.profileService.setUserProfile(this.data, userCod)
+            .subscribe(
+              result => console.log(result)
+            );
+      }
+        if (strCPF === null) {
+          this.alertService.warn('Digite seu CPF');
+        } else if (this.testCPF(strCPF) === false) {
+          this.alertService.error('CPF inválido/Digite um CPF válido');
+        } else {
+          this.alertService.success('CPF válido');
+        }
+      }
+    
+     expectedCPF(strCPF: string) {
+      let soma = '00000000000';
+      for (let i = 0 ; i <= 9 ; i++) {
+        if (strCPF === soma) {
+          return false;
+        }
+        soma = String(11111111111 + parseInt( soma, 10));
+      }
+      if (strCPF === null) {
+        return false;
+      }
+     }
+     calculateCPF(strCPF: string, sum: any, rest: any) {
+      sum = this.sumStringValues(strCPF, sum, 9);
+      rest = (sum * 10) % 11;
+      if ((rest === 10) || (rest === 11)) {
+        rest = 0;
+      }
+      if (rest !== parseInt(strCPF.substring(9, 10))) {
+      return false;
+      }
+    }
+    
+    calculateTwoCPF(strCPF: string, sum: any, rest: any) {
+      sum = this.sumStringValues(strCPF, sum, 10);
+      rest = (sum * 10) % 11;
+      if ((rest === 10) || (rest === 11)) {
+        rest = 0;
+      }
+      if (rest !== parseInt(strCPF.substring(10, 11))) {
+      return false;
+      }
+    }
+    
+    sumStringValues(strCPF: string, sum: any, limite1: any): any {
+      for (let i = 1; i <= limite1; i++) {
+        sum += parseInt(strCPF.substring(i -1, i)) * (limite1 + 2 - i);
+      }
+      return sum;
+    }
+    
+    testCPF(strCPF: string) {
+        let sum;
+        let rest;
+        sum = 0;
+        rest = 0;
+        if (this.expectedCPF(strCPF) === false || this.calculateCPF(strCPF, sum, rest) === false ||
+         this.calculateTwoCPF(strCPF, sum, rest)) {
+           return false;
+          }
+        return true;
+      }
 }
