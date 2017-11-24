6706fb1dce569888ab039b4695a530b55e3ea214
joao4018 <joao4018@gmail.com>
2017-11-24 03:15:57 -0200

    Minor: Test checklist production 87,1%

--- src/app/checklist/checklist-production/checklist-production.component.spec.ts
+++ src/app/checklist/checklist-production/checklist-production.component.spec.ts
@@ -42,30 +42,23 @@ describe('ChecklistProductionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should ngOnInit', () => {
+    component.ngOnInit();
+  });
+  it('should onSubmit', () => {
+    component.onSubmit();
+  });
+
 
   it('should fail incorrect not rquired questions', () => {
-      const invalidQuestionNumber = 4;
-      const invalidTopicNumber = 5;
-      const validQuestionNumber = 0;
-      const validTopicNumber = 6;
+    const teste = component.isNotRequiredQuestions(5,6)
+    expect(teste).toBe(false);
+    const teste2 = component. isRequiredQuestions(3,3)
+    expect(teste2).toBe(false);
+    const teste3 = component.isOtherQuestions(6,4)
+    expect(teste3).toBe(false);
 
-      // First case
-      expect(component.isNotRequiredQuestions(invalidTopicNumber, invalidQuestionNumber)).toBeFalsy();
-
-      // Second case
-      expect(component.isNotRequiredQuestions(invalidTopicNumber, validQuestionNumber)).toBeFalsy();
-
-      // Third case
-      expect(component.isNotRequiredQuestions(validTopicNumber, invalidQuestionNumber)).toBeFalsy();
-  });
-
-  it('should pass correct not required questions', () => {
-    const validQuestionNumber = 0;
-    const validTopicNumber = 6;
-
-    // First case
-    expect(component.isNotRequiredQuestions(validTopicNumber, validQuestionNumber)).toBeTruthy();
-  });
+});
 
   it('should have back button', () => {
     const backButton = compiled.querySelector('.backButton');
