883afff66c885325d456bbac99c463870ab04b68
joao4018 <joao4018@gmail.com>
2017-12-07 17:54:19 -0200

    Minor: bugs(test) solved and initial test

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,3 +1,12 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProfileService } from './../services/profile/profile.service';
+import { Router, RouterModule } from '@angular/router';
+import { AlertService } from './../services/alert/alert.service';
+import { AuthenticationService } from './../services/authentication/authentication.service';
+import { MockBackend } from '@angular/http/testing';
+import { UserService } from './../services/user/user.service';
+import { Http, ConnectionBackend, RequestOptions, HttpModule } from '@angular/http';
+import { NgForm, FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
@@ -5,10 +14,17 @@ import { AdminComponent } from './admin.component';
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [ AdminComponent ],
+      imports: [FormsModule, HttpModule, RouterTestingModule],
+      providers: [
+        UserService,
+        MockBackend,
+        ConnectionBackend,
+        AuthenticationService,
+        ProfileService,
+        AlertService  ]
     })
     .compileComponents();
   }));
@@ -19,7 +35,7 @@ describe('AdminComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
